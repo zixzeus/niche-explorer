32634d4cc53f02b8a72b5e50d644b47e5a527c8b
zixzeus <5369214+zixzeus@user.noreply.gitee.com>
2026-03-13 22:51:19 +0800

    feat: integrate Google Analytics (GA4).发挥视觉优势

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -24,6 +24,21 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&family=Outfit:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />
+        
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-HLTZZDJZWT"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-HLTZZDJZWT', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
       </head>
       <body className="antialiased">
         {children}
